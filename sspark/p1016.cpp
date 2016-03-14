4a183e563fd14c3626a1131fcf68cb9367c3b8c8
ChalkPE <chalkpe@gmail.com>
2016-03-14 09:49:31 +0900

    Added p1016 (pre)

---
+++ sspark/p1016.cpp
@@ -0,0 +1,11 @@
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2016-03-14
+ */
+
+#include <stdio.h>
+int main(){
+    int a = 49; double b = 0.2683;
+    printf("%d * %lf = %lf", a, b, a * b);
+    return 0;
+}
