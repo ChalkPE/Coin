d506849f908f71fbddda8b212bdd27b28f039e43
ChalkPE <chalkpe@gmail.com>
2016-03-16 12:49:38 +0900

    Added p1017

---
+++ sspark/p1017.cpp
@@ -0,0 +1,11 @@
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2016-03-16
+ */
+
+#include <stdio.h>
+int main(){
+    int a; printf("키 = "); scanf("%d", &a);
+    printf("당신의 키는 %dcm입니다.", a);
+    return 0;
+}
