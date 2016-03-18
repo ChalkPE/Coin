8d309cbbd8065a8f1a9a68d9980d57263ce63004
ChalkPE <chalkpe@gmail.com>
2016-03-18 12:04:21 +0900

    Added p1022

---
+++ sspark/p1022.cpp
@@ -0,0 +1,11 @@
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2016-03-18
+ */
+
+#include <stdio.h>
+int main(){
+    int a = 10, b = 20, c = 30;
+    printf("%d + %d = %d", a, b, c);
+    return 0;
+}
