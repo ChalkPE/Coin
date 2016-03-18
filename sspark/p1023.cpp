234798258419afc56da0f6e24318935fabbd48cd
ChalkPE <chalkpe@gmail.com>
2016-03-18 12:13:21 +0900

    Added p1023

---
+++ sspark/p1023.cpp
@@ -0,0 +1,11 @@
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2016-03-18
+ */
+
+#include <stdio.h>
+int main(){
+    double a = 80.5, b = 22.34;
+    printf("%10.2lf%10.2lf%10.2lf", a, b, a + b);
+    return 0;
+}
