ac81e37efda6dac2a11d07341a5e06f0fce7bbab
ChalkPE <chalkpe@gmail.com>
2016-03-24 09:44:56 +0900

    Added p1031

--- sspark/p1030.cpp
+++ sspark/p1030.cpp
@@ -5,9 +5,10 @@
 
 #include <stdio.h>
 int main(){
-    int a, b, c; scanf("%d %d", &a, &b);
-    c = a++ * --b;
-    printf("%d %d %d\n", a, b, c);
+    int a, b, c;
+    scanf("%d %d", &a, &b); //a(10) b(20) 입력
+    c = a++ * --b; //a(10: 대입 후 증가) * b(19: 감소 후 대입) = c(190)
+    printf("%d %d %d\n", a, b, c); //a(11) b(19) c(190) 출력
     return 0;
 }
 
---
+++ sspark/p1031.cpp
@@ -0,0 +1,13 @@
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2016-03-24
+ */
+
+#include <stdio.h>
+int main(){
+    int a, b; scanf("%d %d", &a, &b);
+    printf("%d\n%d", a == b, a != b);
+    return 0;
+}
+
+//END OF FILE
