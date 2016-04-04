e0200ea69f14f7c4c176c330dee84c0ccd505f56
ChalkPE <chalkpe@gmail.com>
2016-04-04 16:05:34 +0900

    Added p1033

---
+++ sspark/p1033.cpp
@@ -0,0 +1,13 @@
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2016-04-04
+ */
+
+#include <stdio.h>
+int main(){
+    int a, b; scanf("%d %d", &a, &b);
+    printf("%d %d", a && b, a || b);
+    return 0;
+}
+
+//END OF FILE
