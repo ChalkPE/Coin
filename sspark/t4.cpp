4de75393f478e6a9b3d74528dd5e6ec12de75a53
ChalkPE <chalkpe@gmail.com>
2016-04-07 09:30:47 +0900

    Added t4

---
+++ sspark/t4.cpp
@@ -0,0 +1,15 @@
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2016-04-07
+ */
+
+#include <stdio.h>
+int main(){
+    int a = 3; a = a + 3; a = a - 3;
+    a = a * 3; a = a / 3; a = a % 3;
+
+    printf("%d", a);
+    return 0;
+}
+
+//END OF FILE
