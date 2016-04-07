b4cc822ce31cf34b4df1be61b28067422141f981
ChalkPE <chalkpe@gmail.com>
2016-04-07 09:50:38 +0900

    Operators!

--- sspark/t4.cpp
+++ sspark/t4.cpp
@@ -8,7 +8,21 @@ int main(){
     int a = 3; a = a + 3; a = a - 3;
     a = a * 3; a = a / 3; a = a % 3;
 
-    printf("%d", a);
+    printf("%d \n \n", a);
+
+    int x = 3, y = 2;
+
+    printf("%d %d \n", x == y, x != y);
+    printf("%d %d \n", x > y, x <= y);
+    printf("%d \n", (x > y) && (x <= y));
+    printf("%d \n", (x > y) || (x <= y));
+    printf("%d \n \n", !(x > y));
+
+    printf("%d %d \n", 3 & 1, 3 | 1);
+    printf("%d \n", 3 ^ 1);
+    printf("%d %d \n", 3 << 1, 3 >> 1);
+    printf("%d \n", ~x);
+
     return 0;
 }
 
