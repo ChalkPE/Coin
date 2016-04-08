b85dd6fbc3a625198ba66bc32776ffd9f2ab7666
ChalkPE <chalkpe@gmail.com>
2016-04-08 12:26:02 +0900

    Added problems

---
+++ sspark/p1036.cpp
@@ -0,0 +1,13 @@
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2016-04-08
+ */
+
+#include <stdio.h>
+int main(){
+    int a, b; scanf("%d %d", &a, &b);
+    printf("%d / %d = %d...%d \n", a, b, a / b, a % b);
+    return 0;
+}
+
+//END OF FILE
---
+++ sspark/p1037.cpp
@@ -0,0 +1,13 @@
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2016-04-08
+ */
+
+#include <stdio.h>
+int main(){
+    int a, b; scanf("%d %d", &a, &b); a += 5; b *= 2;
+    printf("가로 = %d\n세로 = %d\n넓이 = %d\n", a, b, a * b);
+    return 0;
+}
+
+//END OF FILE
