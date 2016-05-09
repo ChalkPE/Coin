b5859630d9a9c019b80054a2bbe91f8e4f8e9982
ChalkPE <chalkpe@gmail.com>
2016-05-09 09:44:00 +0900

    Updated t15

--- sspark/2016/05/t15.cpp
+++ sspark/2016/05/t15.cpp
@@ -8,7 +8,7 @@
 int main(){
     int a; printf("1과목의 점수를 입력하세요: "); scanf("%d", &a);
     int b; printf("2과목의 점수를 입력하세요: "); scanf("%d", &b);
-    printf("%s합격\n", a >= 60 && b >= 60 ? "" : "불");
+    if(a < 60 || b < 60) printf("불"); printf("합격\n");
 }
 
 // END OF FILE
