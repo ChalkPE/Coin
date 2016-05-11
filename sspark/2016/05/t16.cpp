51caad83cf18883e314bd6e0727412d9ce38082b
ChalkPE <chalkpe@gmail.com>
2016-05-11 12:28:18 +0900

    Updated t16

--- sspark/2016/05/t16.cpp
+++ sspark/2016/05/t16.cpp
@@ -4,15 +4,16 @@
  */
 
 #include <stdio.h>
+#define p(format, ...) printf(format, ##__VA_ARGS__)
 
 int main(){
     int s; printf("과목의 점수를 입력하세요: ");
     scanf("%d", &s); switch(s / 10){
-        default: printf(s >= 90 ? "수" : "가");
-        break; case 8: printf("우");
-        break; case 7: printf("미");
-        break; case 6: printf("양");
-    } printf("\n");
+        default: p(s >= 90 ? "수" : "가");
+        break; case 8: p("우");
+        break; case 7: p("미");
+        break; case 6: p("양");
+    } return p("\n") * 0;
 }
 
 // END OF FILE
