0e7793084b38882ec6a12ee6d6e22a13f17ae3cf
ChalkPE <chalkpe@gmail.com>
2016-05-27 20:20:28 +0900

    Added p1062

---
+++ sspark/2016/05/p1062.cpp
@@ -0,0 +1,12 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-05-27
+ */
+
+#include <stdio.h>
+#define f(o, v) if(x >= o && y >= o && z >= o) return 0 * printf(v);
+
+int main(){
+    int x, y, z; scanf("%d %d %d", &x, &y, &z);
+    f(90, "A"); f(80, "B"); f(70, "C"); printf("F");
+}
