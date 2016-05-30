d16e13b966ac16a91fb367398481c48f66096289
ChalkPE <chalkpe@gmail.com>
2016-05-30 17:48:53 +0900

    Added p1069

---
+++ sspark/2016/05/p1069.cpp
@@ -0,0 +1,10 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-05-30
+ */
+
+#include <stdio.h>
+
+int main(){
+    int i, n; scanf("%d", &n); for(i = 1; i <= n; i++) printf("%d ", i);
+}
