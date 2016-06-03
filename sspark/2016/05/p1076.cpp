e8ea627339a381e7a4b9f1d57d614a794f9ff5e3
ChalkPE <chalkpe@gmail.com>
2016-06-03 12:38:07 +0900

    Added p1076

---
+++ sspark/2016/05/p1076.cpp
@@ -0,0 +1,11 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-06-03
+ */
+
+#include <stdio.h>
+
+int main(){
+    int i, n; scanf("%d", &n);
+    for(i = 2; i <= n; i += 2) printf("%d ", i);
+}
