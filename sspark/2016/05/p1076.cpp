bf080c487e4e6430c3cc73bb611da62b96ea64c2
ChalkPE <chalkpe@gmail.com>
2016-06-03 12:40:57 +0900

    Updated p1076

--- sspark/2016/05/p1076.cpp
+++ sspark/2016/05/p1076.cpp
@@ -6,6 +6,5 @@
 #include <stdio.h>
 
 int main(){
-    int i, n; scanf("%d", &n);
-    for(i = 2; i <= n; i += 2) printf("%d ", i);
+    int n, i; for(scanf("%d", &n), i = 2; i <= n; i += 2) printf("%d ", i);
 }
