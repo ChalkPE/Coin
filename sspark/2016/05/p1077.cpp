2d9778fb94cab82468a44a1fe3b880a794d037c9
ChalkPE <chalkpe@gmail.com>
2016-06-08 21:32:46 +0900

    Added p1077

---
+++ sspark/2016/05/p1077.cpp
@@ -0,0 +1,10 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-06-08
+ */
+
+#include <stdio.h>
+
+int main(){
+    int n, s = 0; scanf("%d", &n); for(; n <= 100; n++) s += n; return 0 * printf("%d", s);
+}
