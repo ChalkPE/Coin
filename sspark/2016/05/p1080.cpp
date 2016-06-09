2701e1f0871d66ce5d68f62ec7aa6da510ec44be
ChalkPE <chalkpe@gmail.com>
2016-06-09 09:27:37 +0900

    Added p1080

---
+++ sspark/2016/05/p1080.cpp
@@ -0,0 +1,10 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-06-09
+ */
+
+#include <stdio.h>
+
+int main(){
+    int i, j; for(i = 1; i <= 5; printf("\n"), i++) for(j = 1; j <= 5; j++) printf("%d ", i + j);
+}
