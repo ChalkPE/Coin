e56ef12df8c1c58032a2a92b92858d83ffe5fbc5
ChalkPE <chalkpe@gmail.com>
2016-07-01 12:51:21 +0900

    Added p1105

---
+++ sspark/2016/07/p1105.c
@@ -0,0 +1,12 @@
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2016-07-01
+ */
+
+#include <stdio.h>
+int main(){
+    int n, x, y, a, b;
+    for(scanf("%d", &n), a = 'A', b = 0, y = 0; y < n; y++, printf("\n")) for(x = 0; x < n; x++, x + y < n ? printf("%c ", a++) : printf("%d ", b++));
+}
+
+//END OF FILE
