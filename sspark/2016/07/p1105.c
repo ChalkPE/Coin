35776a9cbd453ab6431a6325c49c1b432a83c06f
ChalkPE <chalkpe@gmail.com>
2016-07-01 12:55:08 +0900

    Oops

--- sspark/2016/07/p1105.c
+++ sspark/2016/07/p1105.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 int main(){
     int n, x, y, a, b;
-    for(scanf("%d", &n), a = 'A', b = 0, y = 0; y < n; y++, printf("\n")) for(x = 0; x < n; x++, x + y < n ? printf("%c ", a++) : printf("%d ", b++));
+    for(scanf("%d", &n), a = 'A', b = 0, y = 0; y < n; y++, printf("\n")) for(x = 0; x < n; x++, x + y <= n ? printf("%c ", a++) : printf("%d ", b++));
 }
 
 //END OF FILE
