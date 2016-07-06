5ba00dd8ff90b593b237151c38ac907687c78d40
ChalkPE <chalkpe@gmail.com>
2016-07-06 20:34:52 +0900

    Added p1111

---
+++ sspark/2016/07/p1111.c
@@ -0,0 +1,11 @@
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2016-07-04
+ */
+
+#include <stdio.h>
+
+int main(){
+    int n, a, b, c; scanf("%d", &n);
+    for(a = 1; a <= n; a++) for(b = a + 1; b <= n; b++) for(c = b + 1; c <= n; c++) if(a * a + b * b == c * c) printf("%d %d %d\n", a, b, c);
+}
