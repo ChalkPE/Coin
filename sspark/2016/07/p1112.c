28d400b259dcca534fccf84bbcd710efd6e22340
ChalkPE <chalkpe@gmail.com>
2016-07-06 20:51:25 +0900

    Small changes

--- sspark/2016/07/p1112.c
+++ sspark/2016/07/p1112.c
@@ -1,7 +1,7 @@
 /**
- * @author ChalkPE <chalkpe@gmail.com>
+ * @author ChalkPE <chalk@chalk.pe>
  * @since 2016-07-06
  */
 
 #include <stdio.h>
-int main() { int n, m; while(scanf("%d %d", &n, &m), n || m) printf("%d %d %d %d \n", n + m, n < m ? m - n : n - m, n * m, n / m); }
+int main(){ int n, m; while(scanf("%d %d", &n, &m), n || m) printf("%d %d %d %d \n", n + m, n < m ? m - n : n - m, n * m, n / m); }
