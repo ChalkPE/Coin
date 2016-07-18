b143bec6337c9c6f7d517124db46190838479cda
ChalkPE <chalkpe@gmail.com>
2016-07-18 18:56:51 +0900

    Yay

--- sspark/2016/07/t02.c
+++ sspark/2016/07/t02.c
@@ -5,7 +5,7 @@
 
 #include <stdio.h>
 
-int main(){
-    char a[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; int n, x, y, w = 0, v = 1;
-    for(scanf("%d", &n), y = n - 1; y < n; y -= v = !y ? -v : v, printf("\n")) for(x = 0; x <= y; x++, printf("%c", a[w++]));
+int main(int n, int x, int y, int w, int v){
+    char a[54] = " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    for(w = v = 1, scanf("%d", &n), y = n - 1; y < n; y -= v = !y ? -v : v, printf("\n")) for(x = 0; x <= y; printf("%c", a[x++, w++]));
 }
