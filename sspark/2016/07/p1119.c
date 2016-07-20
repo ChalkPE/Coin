92c08af447ca0790a89fd48a357c11305d5f1e67
ChalkPE <chalkpe@gmail.com>
2016-07-20 12:31:34 +0900

    Added p1119

---
+++ sspark/2016/07/p1119.c
@@ -0,0 +1,10 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-07-20
+ */
+
+#include <stdio.h>
+int main() {
+    int a[100], i, j;
+    for(i = 0; i < 100; i++) if(scanf("%d", a + i), !a[i]) { for(j = i - 1; j >= 0; printf("%d ", a[j--])); return 0; }
+}
