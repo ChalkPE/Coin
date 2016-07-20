1798b154505d35643604a70aed495f898ecf6cf7
ChalkPE <chalkpe@gmail.com>
2016-07-20 12:09:10 +0900

    Added p1118

---
+++ sspark/2016/07/p1118.c
@@ -0,0 +1,11 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-07-20
+ */
+
+#include <stdio.h>
+int main(){
+    char a[7], i;
+    for(i = 0; i < 7; scanf("%c ", a + i++));
+    return 0 * printf("%c %c %c", a[0], a[3], a[6]);
+}
