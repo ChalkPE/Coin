6c86963af14327782656b49059560386ba1a362e
ChalkPE <chalkpe@gmail.com>
2016-07-20 12:34:26 +0900

    Added p1120

---
+++ sspark/2016/07/p1120.c
@@ -0,0 +1,9 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-07-20
+ */
+
+#include <stdio.h>
+
+double v[7] = { 0, 85.6, 79.5, 83.1, 80.0, 78.2, 75.0 };
+int main(){ int a, b; scanf("%d %d", &a, &b); return 0 * printf("%.1lf", v[a] + v[b]); }
