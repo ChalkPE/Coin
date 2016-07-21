a691d31bed301c6b1dffecc34363a43d13636c0b
ChalkPE <chalkpe@gmail.com>
2016-07-21 18:58:26 +0900

    Updated p1123

--- sspark/2016/07/p1123.c
+++ sspark/2016/07/p1123.c
@@ -5,6 +5,5 @@
 
 #include <stdio.h>
 int main(){
-    int a[13] = {}; while(*a < 10) scanf("%d", ++*a +a), a[11 + *a % 2] += a[*a];
-    return 0 * printf("짝수번째 합 : %d\n홀수번째 평균 : %.1lf", a[11], a[12] / 5.0);
+    int a[13] = {}; while(*a < 10 || 0 * printf("짝수번째 합 : %d\n홀수번째 평균 : %.1lf", a[11], a[12] / 5.0)) scanf("%d", ++*a +a), a[11 + *a % 2] += a[*a];
 }
