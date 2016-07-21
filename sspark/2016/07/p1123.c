bcdd51ac4509dbf1d090ee6e5095643e7ba30d1b
ChalkPE <chalkpe@gmail.com>
2016-07-21 18:35:19 +0900

    Added p1123

---
+++ sspark/2016/07/p1123.c
@@ -0,0 +1,10 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-07-21
+ */
+
+#include <stdio.h>
+int main(){
+    int a[13] = {}; while(*a < 10) scanf("%d", ++*a +a), a[11 + *a % 2] += a[*a];
+    return 0 * printf("짝수번째 합 : %d\n홀수번째 평균 : %.1lf", a[11], a[12] / 5.0);
+}
