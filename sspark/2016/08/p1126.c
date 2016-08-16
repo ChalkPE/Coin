d1df414317cda6a0c9f865c710cebf17736d9193
ChalkPE <chalk@chalk.pe>
2016-08-16 10:36:20 +0900

    Added problems

---
+++ sspark/2016/08/p1126.c
@@ -0,0 +1,10 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-08-16
+ */
+
+#include <stdio.h>
+int main(){
+    int a[7];
+    for(a[6] = *a = 0; *a < 5 || 0 * printf("%d", a[6]); scanf("%d", ++*a +a), a[6] += *a % 2 * a[*a]);
+}
---
+++ sspark/2016/08/p1127.c
@@ -0,0 +1,10 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-08-16
+ */
+
+#include <stdio.h>
+int main(){
+    int a[11], v[2] = {0, 0};
+    for(*a = 0; *a < 10 || 0 * printf("홀수 번째 합 : %d\n짝수 번째 합 : %d\n", v[1], v[0]); scanf("%d", ++*a +a), v[*a % 2] += a[*a]);
+}
