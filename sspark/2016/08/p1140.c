37aaaa2baa7d812a8bb6f2c9188968a7a5938b1f
ChalkPE <chalk@chalk.pe>
2016-08-26 16:22:28 +0900

    Added p1140

---
+++ sspark/2016/08/p1140.c
@@ -0,0 +1,7 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-08-26
+ */
+
+#include <stdio.h>
+int main(){ int v, w, x, y, z; for(z = y = 0; y < 5 || 0 * printf("합격자 %d명\n", z); y++) for(v = x = 0; x < 4 || 0 * printf("%s합격\n", v < 320 ? "불" : (z++, "")); x++) scanf("%d", &w), v += w; }
