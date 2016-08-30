ba741d65029a15087c24582b7e4d787e3eee284a
ChalkPE <chalk@chalk.pe>
2016-08-30 10:14:55 +0900

    Reduced loop count

--- sspark/2016/08/p1144.c
+++ sspark/2016/08/p1144.c
@@ -5,7 +5,6 @@
 
 #include <stdio.h>
 int main(){
-    int a[11]; scanf("%d %d", a + 1, a + 2);
-    for(*a = 3; *a <= 10; ++*a) a[*a] = (a[*a - 1] + a[*a - 2]) % 10;
-    for(*a = 1; *a <= 10; ++*a) printf("%d ", a[*a]);
+    int a[11]; scanf("%d %d", a + 1, a + 2), printf("%d %d", a[1], a[2]);
+    for(*a = 3; *a <= 10; ++*a) printf(" %d", a[*a] = (a[*a - 1] + a[*a - 2]) % 10);
 }
