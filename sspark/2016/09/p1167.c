d85021b5b6f4d541862805bcb398ff3e9b48bc24
ChalkPE <chalk@chalk.pe>
2016-09-12 15:27:37 +0900

    Updated p1167

--- sspark/2016/09/p1167.c
+++ sspark/2016/09/p1167.c
@@ -1,23 +1,15 @@
 /**
-* @author ChalkPE <chalk@chalk.pe>
-* @since 2016-09-12
-*/
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-09-12
+ */
 
 #include <stdio.h>
 
 int f(int a, char o, int b){
-    switch(o){
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
-        case '/': return a / b;
-        default:  return 0;
-    }
+    if(o == '+') return a + b; if(o == '-') return a - b;
+    if(o == '*') return a * b; if(o == '/') return a / b; return 0;
 }
 
 int main(){
-    int a, b; char o;
-
-    scanf("%d %c %d", &a, &o, &b);
-    printf("%d %c %d = %d", a, o, b, f(a, o, b));
+    int a, b; char o; scanf("%d %c %d", &a, &o, &b), printf("%d %c %d = %d", a, o, b, f(a, o, b));
 }
