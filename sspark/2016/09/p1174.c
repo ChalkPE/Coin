ed735029af0a8d35b1bfffc25e30b256bdf8c558
ChalkPE <chalk@chalk.pe>
2016-09-22 15:41:14 +0900

    Added p1174

---
+++ sspark/2016/09/p1174.c
@@ -0,0 +1,16 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-09-22
+ */
+
+#include <stdio.h>
+
+void write(int a[]){
+    int k; for(k = 0; k < 10 || 0 * printf("\n"); printf("%d ", a[k++]));
+}
+
+int main(){
+    int i, j, k, a[10];
+    for(i = 0; i < 10; scanf("%d", a+ i++));
+    for(i = 9; i > 0; i--, write(a)) for(j = 0; j < i; j++) if(a[j] < a[j + 1]) k = a[j], a[j] = a[j + 1], a[j + 1] = k;
+}
