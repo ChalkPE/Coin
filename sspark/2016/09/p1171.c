ddc8a176ebd60b2789174915467d58649cf157a6
ChalkPE <chalk@chalk.pe>
2016-09-26 15:39:03 +0900

    Added p1171

---
+++ sspark/2016/09/p1171.c
@@ -0,0 +1,11 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-09-26
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int d; scanf("%d", &d), printf("%.2lf\n", sqrt(d / 3.14));
+}
