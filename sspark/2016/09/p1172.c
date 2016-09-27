832e865fbd54633635219406641579a7146b2c3f
ChalkPE <chalk@chalk.pe>
2016-09-27 10:56:27 +0900

    Updated!

--- sspark/2016/09/p1172.c
+++ sspark/2016/09/p1172.c
@@ -6,9 +6,7 @@
 #include <math.h>
 #include <stdio.h>
 
-#define swap(a, b) if(a < b) v[3] = a, a = b, b = v[3]
-
 int main(){
-    int i, j; double v[4]; for(i = 0; i < 3; scanf("%lf", v+ i++));
-    for(i = 0; i < 2 || 0 * printf("%.0lf %.0lf %.0lf\n", ceil(v[0]), floor(v[2]), floor(v[1] + 0.5)); i++) for(j = i + 1; j < 3; j++) swap(v[i], v[j]);
+    int i, j; double v[4];
+    for(scanf("%lf %lf %lf", v, v + 1, v + 2), i = 0; i < 2 || 0 * printf("%.0lf %.0lf %.0lf\n", ceil(v[0]), floor(v[2]), floor(v[1] + 0.5)); i++) for(j = i + 1; j < 3; j++) if(v[i] < v[j]) v[3] = v[i], v[i] = v[j], v[j] = v[3];
 }
