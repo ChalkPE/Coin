92b24b21b076fdcfe83f065a42c75b5e3a9b015f
ChalkPE <chalk@chalk.pe>
2016-10-04 10:10:18 +0900

    Added p1178

---
+++ sspark/2016/10/p1178.c
@@ -0,0 +1,9 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-10-04
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int r, int i, int x){ for(r = i = 0; i < 5 || 0 * printf("%d", r); i++) scanf("%d", &x), r += abs(x); }
