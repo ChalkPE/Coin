95d29d89dd0419d7581caa854d9cad3e6c9952b9
ChalkPE <chalk@chalk.pe>
2016-10-04 10:16:07 +0900

    Added p1179

---
+++ sspark/2016/10/p1179.c
@@ -0,0 +1,9 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-10-04
+ */
+
+#include <math.h>
+#include <stdio.h>
+
+int main(int n){ return scanf("%d", &n), 0 * printf("%d", (int) pow(2, n)); }
