836ad17a00011b99308a1720c01c9819e2b53995
ChalkPE <chalk@chalk.pe>
2016-11-01 10:47:03 +0900

    Added p1200

---
+++ sspark/2016/10/p1200.c
@@ -0,0 +1,9 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-11-01
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){ char c; while(scanf(" %c", &c), isalnum(c)) printf((isalpha(c) ? "%c\n" : "%d\n"), c); }
