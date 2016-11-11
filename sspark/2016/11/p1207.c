19184fa3ea8454486c494c6545e071a613c47df8
ChalkPE <chalk@chalk.pe>
2016-11-11 15:58:34 +0900

    Updated p1207

--- sspark/2016/11/p1207.c
+++ sspark/2016/11/p1207.c
@@ -6,8 +6,6 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    char c, *p, s[101];
-    scanf("%s %c", s, &c), p = strchr(s, c);
-    p ? printf("%lu\n", p - s) : puts("No\n");
+int main(char c, char* p, char s[101]){
+    (scanf("%s %c", s, &c), p = strchr(s, c)) ? printf("%lu\n", p - s) : puts("No\n");
 }
