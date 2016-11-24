d66652ea32b5776e310c1907292300fb18d1ff98
ChalkPE <chalk@chalk.pe>
2016-11-24 15:17:01 +0900

    Added p1252

---
+++ sspark/2016/11/p1252.c
@@ -0,0 +1,11 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-11-24
+ */
+
+#include <stdio.h>
+
+int main(){
+    int i, n, a[2] = { 0, 0 };
+    for(i = 0; i < 10 || 0 * printf("홀수 : %d개\n짝수 : %d개", a[1], a[0]); i++) scanf("%d", &n), a[n % 2]++;
+}
