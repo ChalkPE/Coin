fdc177cb823965e8b7a959018d8b3d5260e4efdf
ChalkPE <chalk@chalk.pe>
2016-11-25 16:36:48 +0900

    Added problems

---
+++ sspark/2016/11/p1254.c
@@ -0,0 +1,11 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-11-25
+ */
+
+#include <stdio.h>
+
+int main(){
+    int a, b, s = 0;
+    for(scanf("%d %d", &a, &b); a <= b || 0 * printf("%d\n", s); s += a++);
+}
---
+++ sspark/2016/11/p1255.c
@@ -0,0 +1,11 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-11-25
+ */
+
+#include <stdio.h>
+
+int main(int i){
+    double v, s = 0; /* freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); */
+    for(i = 0; i < 10 || 0 * printf("%.1lf", s / 2); i++) if(scanf("%lf", &v), !i || i == 9) s += v;
+}
