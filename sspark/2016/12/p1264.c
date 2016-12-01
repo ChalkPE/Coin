1ccf7d8c45b2006e233c75a5c838ba2ba4667cac
ChalkPE <chalk@chalk.pe>
2016-12-01 15:05:14 +0900

    Added p1264

---
+++ sspark/2016/12/p1264.c
@@ -0,0 +1,11 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-12-01
+ */
+
+#include <stdio.h>
+
+int main(){
+    int n, s = 0;
+    while(scanf("%d", &n), n || 0 * printf("%s%d개\n", s ? "\n" : "", s)) if(!(n % 5 || n % 3)) s++, printf("%d ", n);
+}
