14c24ff2f49ed5cf2bdf10f51cf8dbecd0b3147b
ChalkPE <chalk@chalk.pe>
2016-12-01 15:43:42 +0900

    Added p1265

---
+++ sspark/2016/12/p1265.c
@@ -0,0 +1,7 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-12-01
+ */
+
+#include <stdio.h>
+int main(int n, int x, int y, int d){ for(scanf("%d", &n), d = y = 1; y; y += (d = y < n ? d : -d), printf("\n")) for(x = 1; x < y + n; x++) printf(x + y > n ? "* " : "  "); }
