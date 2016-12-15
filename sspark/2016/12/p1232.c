1b7d50ac139fca1577623520328be8fdc8059b66
ChalkPE <chalk@chalk.pe>
2016-12-15 15:06:14 +0900

    Added p1232

---
+++ sspark/2016/12/p1232.c
@@ -0,0 +1,9 @@
+/**
+ * @author ChalkPE <chalk@chalk.pe>
+ * @since 2016-12-05
+ */
+
+#include <stdio.h>
+#define print(x) printf("%s %d학년\n", x.name, x.grade)
+struct { char name[1024]; int grade; } a = { "제주초등학교", 6 }, b;
+int main(){ return scanf("%s %d", b.name, &b.grade), print(a), print(b), 0; }
